36eed837dc91821411ea083a3a2ac4b95df04638
Ref-Bit <rsv.gho@gmail.com>
2020-05-09 18:09:12 +0300

    Updated CSS

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => {
   return (
     <React.Fragment>
-      <footer className="my-2">
+      <footer>
         <h3 className="text-center text-lg font-semibold">Recipe Act | Created by <span> </span> 
           <a href="https://github.com/ref-bit" className="text-orange-700" target="_blank" rel="noopener noreferrer">Ref-Bit</a>
         </h3>
